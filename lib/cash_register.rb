66dfe5e097574dc6f2c22f06869225f3d3e05c0d
Evan Rosson <rossonevan7281@gmail.com>
2022-08-09 16:07:38 -0500

    commit tree 3054bbf8f34db07399f962900402ca8fcdc38e04

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
+require 'pry'
 
+class CashRegister
+
+    attr_accessor :total, :discount, :title, :price
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+    end
+
+    def add_item(quantity = 0)
+        @title = title
+        @price = price
+        @quantity = quantity
+        self.total += {price * quantity}
+    end
+
+end
